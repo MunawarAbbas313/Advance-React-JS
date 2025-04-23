a1652e0166c25271cecf65c86c727a6f477ec7b6
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-04-23 22:00:14 +0500

    Create DesserList.jsx

---
+++ Example/DesserList.jsx
@@ -0,0 +1,20 @@
+function DessertsList(props) {
+    return (
+      <>
+        <ul>
+          {
+            props.data.map((item, index) => (
+              <li key={item.id || index}>
+                <p><strong>Name:</strong> {item.name}</p>
+                <p><strong>Calories:</strong> {item.calories}</p>
+                <p><strong>Created At:</strong> {item.createdAt}</p>
+              </li>
+            ))}
+        </ul>
+      </>
+    );
+  }
+export default DessertsList;  
+
+// here its returning all the list items 
+
