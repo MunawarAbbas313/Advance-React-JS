c302be90d14084856847bf21cd9a358e9dfaf07a
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-04-25 21:01:17 +0500

    Create KeysPratic.jsx

---
+++ KeysInReact/KeysPratic.jsx
@@ -0,0 +1,86 @@
+/* App.jsx */
+import { useState } from "react";
+import Todo from "./Todo";
+
+function App(){
+  const [todo , setToto] = useState([
+    {
+      id: "todo 1",
+      CreatedAt: "25-April-2025",
+      val: "PayFirst"
+    },
+    {
+      id: "todo 2",
+      CreatedAt: "26-April-2025",
+      val:"Buy first"
+    }
+  ]);
+
+  const revereTodo = ()=>{
+    setToto([...todo].reverse());
+  }
+      return(
+        <>
+        <button
+          onClick={revereTodo}
+          style={{
+        backgroundColor: "blue",
+        color: "white",
+        padding: "10px 20px",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+        marginBottom: "10px"
+          }}
+        >Reverse</button>
+        <table style={{ borderCollapse: "collapse", width: "100%" }}>
+          <tbody>
+        {todo.map((todo, index) => (
+          <Todo 
+        id={todo.id} 
+        val= {todo.val}
+        CreatedAt={todo.CreatedAt} 
+        
+      key={index}
+        style={{
+          border: "1px solid black",
+          padding: "8px",
+          textAlign: "left"
+        }}
+          />
+        ))}
+     
+          </tbody>
+        </table>
+        </>
+      )
+
+}
+export default App;
+
+
+
+/* Todo.jsx*/
+
+function Todo(props) {
+    return (
+        <>
+            <tr style={{ backgroundColor: "#f9f9f9", borderBottom: "1px solid #ddd", textAlign: "left"  }}>
+                <td style={{ padding: "10px", fontWeight: "bold" }}>
+                    
+                    <label>{props.id}</label>
+                    </td>
+                <td style={{ padding: "10px" }}>
+                    <input style={{ padding: "5px", borderRadius: "4px", border: "1px solid #ccc" }} 
+                    value={props.val || "No Value"}
+              
+                    />
+                </td>
+                <td style={{ padding: "10px", color: "#555" }}>
+                    <label>{props.CreatedAt}</label>
+                </td>
+            </tr>
+        </>
+    );
+}
+export default Todo;
