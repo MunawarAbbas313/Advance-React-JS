2a7f05b13869edc3c34380a9a7daf43e6bf2f514
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-04-26 21:50:12 +0500

    Update FeedbackDet.jsx

--- Forms/FeedbackFormPratice/FeedbackDet.jsx
+++ Forms/FeedbackFormPratice/FeedbackDet.jsx
@@ -1,1 +1,16 @@
-
+function FeedbackDet(props){
+    return (
+        <>
+            <h2 style={{ color: 'blue', fontFamily: 'Arial, sans-serif', marginBottom: '10px' }}>
+                Name of Customer: {props.name}
+            </h2>
+            <h3 style={{ color: 'green', fontFamily: 'Verdana, sans-serif', marginBottom: '8px' }}>
+                Rating Given: {props.Score}
+            </h3>
+            <p style={{ color: 'gray', fontFamily: 'Tahoma, sans-serif', lineHeight: '1.5' }}>
+                Comment: {props.Comment}
+            </p>
+        </>
+    )
+}
+export default FeedbackDet;
