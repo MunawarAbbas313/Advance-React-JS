d0160b4fb6599c2bcc7cae35efdd0c86334eb67e
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-04-26 21:49:46 +0500

    Update and rename App,jsx to App.jsx

---
+++ Forms/FeedbackFormPratice/App.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import FeedbackDet from "./FeedbackDet";
+
+function App(){
+  const [Score , SetScore] = useState(10);
+  const [Name , SetName] = useState("");
+  const [Comment , SetCommant] = useState("");
+  const [shoeData , setShowData] = useState(false);
+  const HandleSubmit = (e)=>{
+    e.preventDefault();
+    if(Number(Score) < 5 || Comment.length <=10 || Name.length < 5)
+      {alert("Kindly Enter a Valid Value ")
+         return;
+      }
+      else{
+        alert("Bro Submitted the From ")
+        setShowData(true)
+      }
+
+  }
+  return (
+    <>
+      <div
+        className="fromBox"
+        style={{
+          maxWidth: "400px",
+          margin: "20px auto",
+          padding: "20px",
+          border: "1px solid #ccc",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          backgroundColor: "#f9f9f9",
+          fontFamily: "Arial, sans-serif",
+        }}
+      >
+        <h2
+          style={{
+            textAlign: "center",
+            color: "#333",
+            marginBottom: "20px",
+          }}
+        >
+          FeedBack Form
+        </h2>
+         {(!shoeData)? (
+          <form onSubmit={HandleSubmit}>
+          <label
+            style={{
+              display: "block",
+              marginBottom: "10px",
+              fontWeight: "bold",
+              color: "#555",
+            }}
+          >
+            Score:{Score} ⭐
+          </label>
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={Score}
+            onChange={(e)=> SetScore(e.target.value)}
+            style={{
+              width: "100%",
+              marginBottom: "15px",
+            }}
+          />
+          <textarea
+          value={Comment}
+          onChange={(e)=> SetCommant(e.target.value)}
+            style={{
+              width: "100%",
+              height: "80px",
+              marginBottom: "15px",
+              padding: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              resize: "none",
+            }}
+          />
+          <input type="text"
+                placeholder="Enter your Name"
+                value={Name}
+                onChange={(e)=> SetName(e.target.value)}
+                style={{
+                  textAlign: "center",
+                  color: "#333",
+                  marginBottom: "20px",
+                  padding:"10px",
+                  fontSize:"12px"
+                }}
+          />
+          <button
+            style={{
+              display: "block",
+              width: "100%",
+              padding: "10px",
+              backgroundColor: "#007BFF",
+              color: "#fff",
+              border: "none",
+              borderRadius: "4px",
+              cursor: "pointer",
+              fontWeight: "bold",
+            }}
+          >
+            Submit
+          </button>
+        </form>
+         ):(
+          <FeedbackDet name= {Name} Comment= {Comment} Score={Score}/>
+         )}
+      </div>
+     
+    </>
+  );
+}
+export default App;
