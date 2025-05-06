46818a9cd5b21567122b4d279f7c1a63452e2adc
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-05-07 00:32:23 +0500

    Update App.jsx

--- CONTEXT/Project/App.jsx
+++ CONTEXT/Project/App.jsx
@@ -1,1 +1,16 @@
+import { UserProvider, UseUser } from "./UserContext";
+
+const LoginedUser = () => {
+  const { user } = UseUser();
+  return (
+    <p style={{ 
+      fontSize: '18px', 
+      color: '#2c3e50',
+      fontWeight: 'bold',
+      margin: '0'
+    }}>
+      Hello : {user.name}
+    </p>
+  );
+};
 
