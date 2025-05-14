0a3f52f71de383483512268d27e561576fd48c62
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-05-14 17:23:46 +0500

    Update App.jsx

--- Mini Project/App.jsx
+++ Mini Project/App.jsx
@@ -1,1 +1,14 @@
+import { useState } from "react";
+
+function App() {
+    const [greeting, setGreeting] = useState({ greetMsg: "HELLO WORLD" });
+    const [theme, setTheme] = useState("light");
+    const [fontSize, setFontSize] = useState(28);
+
+    const UpdateGreet = () => {
+        setGreeting({
+            greetMsg:
+                greeting.greetMsg === "HELLO WORLD" ? "Good Bye World" : "HELLO WORLD",
+        });
+    };
 
