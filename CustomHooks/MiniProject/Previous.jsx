4614fc88008897eb41204d58af793f3a58d9d24b
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-05-19 17:49:55 +0500

    Update Previous.jsx

--- CustomHooks/MiniProject/Previous.jsx
+++ CustomHooks/MiniProject/Previous.jsx
@@ -1,1 +1,13 @@
+import { useState, useEffect, useRef } from "react";
 
+// ✅ Step 1: Create the custom hook
+function usePrevious(val) {
+  const ref = useRef(); // create a mutable ref
+
+  useEffect(() => {
+    ref.current = val; // update ref after each render
+  }, [val]); // only when `val` changes
+
+  return ref.current; // return the previous value
+}
+export default usePrevious;
